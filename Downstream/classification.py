edd2671d1f0c3a38766fffaa5dc0c52a9c3b5c2b
P Balaramakrishna Varma <balaramakrsihna.p@studnets.iiit.ac.in>
2023-09-24 19:35:29 +0530

    Model code written

--- Downstream/classification.py
+++ Downstream/classification.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.insert(0, './ELMOv/')
 from LM import load_glove
+from elmov import Elmov
 
 class ClassificationDataset(Dataset):
     def __init__(self, file_path, vocab):
@@ -47,10 +48,37 @@ def custom_collate(batch):
     return Sentences, Classes
 
 
+class ClassificationModel(nn.Module):
+    def __init__(self, forward_file, backward_file, len_vocab, gobal_embedding, w1=None):
+        super().__init__()
+        self.pre_trained = Elmov(forward_file, backward_file, len_vocab, gobal_embedding, w1)
+        self.meaning_aggr = nn.LSTM(input_size=200, hidden_size=200, batch_first=True)
+        self.hidden = nn.Linear(200, 30)
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(30, 4)
+    
+    def forward(self, x):
+        # word embedding
+        x = self.pre_trained(x)
+        
+        # meaning aggregation
+        x, hidden = self.meaning_aggr(x)
+        meaning = hidden[0].reshape((x.shape[0], -1))
+        # print(meaning.shape)
+        
+        # MLP classifier
+        hidden_space = self.hidden(meaning)
+        hidden_space = self.relu(hidden_space)
+        logits = self.classifier(hidden_space)
+        return logits
+
+
 if __name__ == "__main__":
     device = "cuda:0"
     global_embeddin, vocab = load_glove(device)
     dataset = ClassificationDataset('./Dataset/AGTokenizedData/test.json', vocab)
-    dataloader = DataLoader(dataset, batch_size=10, shuffle=True, collate_fn=custom_collate)
+    dataloader = DataLoader(dataset, batch_size=20, shuffle=True, collate_fn=custom_collate)
+    Model = ClassificationModel('./fwmodel.pt', './bwmodel.pt', len(vocab), global_embeddin).to(device)
     for X, y in dataloader:
-        print(X.shape, y.shape)
+        X, y = X.to(device), y.to(device)
+        Model(X)
