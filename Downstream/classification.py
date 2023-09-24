205466afe50b90b4f041e7e50c27ca60239b583a
P Balaramakrishna Varma <balaramakrsihna.p@studnets.iiit.ac.in>
2023-09-24 19:13:11 +0530

    Data loading entire data processing is done

--- Downstream/classification.py
+++ Downstream/classification.py
@@ -2,6 +2,7 @@ import string, json
 import torch
 import torchtext
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 import torch.nn as nn
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -34,14 +35,22 @@ class ClassificationDataset(Dataset):
                 X.append(self.vocab[token])
             else:
                 X.append(self.vocab['<unk>'])
+        # return tokens, y
         return torch.tensor(X), torch.tensor(y)
     
+    
+def custom_collate(batch):
+    Sentences = [sample[0] for sample in batch]
+    Classes = torch.tensor([sample[1] for sample in batch])
+
+    Sentences = pad_sequence(Sentences, batch_first=True)
+    return Sentences, Classes
 
 
 if __name__ == "__main__":
     device = "cuda:0"
     global_embeddin, vocab = load_glove(device)
     dataset = ClassificationDataset('./Dataset/AGTokenizedData/test.json', vocab)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    dataloader = DataLoader(dataset, batch_size=10, shuffle=True, collate_fn=custom_collate)
     for X, y in dataloader:
         print(X.shape, y.shape)
